907dd025a74126fa0848d9bfb3b97ba6864c7130
ParkDH0809 <znxn1237@gmail.com>
2023-09-19 16:42:05 +0900

    이름 입력 추가

--- app/src/main/java/telephone/constants/ExceptionMessage.java
+++ app/src/main/java/telephone/constants/ExceptionMessage.java
@@ -3,8 +3,8 @@ package telephone.constants;
 public enum ExceptionMessage {
     SELECT_NUMBER_LESS_THAN_MINIMUM_NUMBER("1 이상의 값을 선택해주세요."),
     SELECT_NUMBER_OVER_THAN_MAXIMUM_NUMBER("3 이하의 값을 선택해주세요."),
-    MISS_INPUT_TELEPHONE_NUMBER("전화번호를 잘못 입력하였습니다.");
-
+    MISS_INPUT_TELEPHONE_NUMBER("전화번호를 잘못 입력하였습니다."),
+    INPUT_NAME_IS_NULL("성함을 적어주세요.");
     private final String exceptionMessage;
 
     ExceptionMessage(String exceptionMessage) {
--- app/src/main/java/telephone/validation/InputValidation.java
+++ app/src/main/java/telephone/validation/InputValidation.java
@@ -9,6 +9,23 @@ public class InputValidation {
     private static final int MINIMUM_SELECT_NUMBER = 1;
     private static final int MAXIMUM_SELECT_NUMBER = 3;
 
+    public static int validateSelectNumber(int number) {
+        if(number < MINIMUM_SELECT_NUMBER) {
+            ExceptionMessage.SELECT_NUMBER_LESS_THAN_MINIMUM_NUMBER.throwException();
+        }
+
+        if(number > MAXIMUM_SELECT_NUMBER) {
+            ExceptionMessage.SELECT_NUMBER_OVER_THAN_MAXIMUM_NUMBER.throwException();
+        }
+        return number;
+    }
+
+    public static String validateName(String name) {
+        if(name == null) {
+            ExceptionMessage.INPUT_NAME_IS_NULL.throwException();
+        }
+        return name;
+    }
 
     public static String validateTelephoneNumber(String number) {
         Pattern pattern = Pattern.compile("^\\d{3}-\\d{3,4}-\\d{4}$");
@@ -18,17 +35,6 @@ public class InputValidation {
             ExceptionMessage.MISS_INPUT_TELEPHONE_NUMBER.throwException();
         } 
 
-        return number;
-    }
-
-    public static int validateSelectNumber(int number) {
-        if(number < MINIMUM_SELECT_NUMBER) {
-            ExceptionMessage.SELECT_NUMBER_LESS_THAN_MINIMUM_NUMBER.throwException();
-        }
-
-        if(number > MAXIMUM_SELECT_NUMBER) {
-            ExceptionMessage.SELECT_NUMBER_OVER_THAN_MAXIMUM_NUMBER.throwException();
-        }
         return number;
     }
 }
